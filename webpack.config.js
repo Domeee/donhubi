3615b211614c55422f45415af094e1ef1b788f46
Dominique Huber <dominique@donhubi.ch>
2018-01-31 21:42:24 +0100

    Remove vh/vw polyfill and migrate to rem units

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
@@ -6,39 +6,40 @@ const extractSass = new ExtractTextPlugin({
 });
 
 module.exports = {
-  entry: [
-    './site/scripts/index.js',
-    './site/styles/index.scss'
-  ],
+  entry: ["./site/styles/index.scss"],
   output: {
-    filename: 'donhubi.js',
-    path: path.resolve(__dirname, 'build')
+    filename: "donhubi.js",
+    path: path.resolve(__dirname, "build")
   },
   devtool: "source-map",
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: extractSass.extract({
-        use: [{
-          loader: "css-loader",
-          options: {
-            sourceMap: true
-          }
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true
-          }
-        }, {
-          loader: "sass-loader",
-          options: {
-            sourceMap: true
-          }
-        }]
-      })
-    }]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }
+    ]
   },
-  plugins: [
-    extractSass
-  ]
+  plugins: [extractSass]
 };

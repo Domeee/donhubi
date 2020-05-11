b4d2c1ddcbca7f53af62d8239ad2612d1238e3a6
Dominique Huber <dominique@donhubi.ch>
2020-05-11 19:02:58 +0200

    Support font preloading

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-        use: ["file-loader"],
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "assets",
+          },
+        },
       },
     ],
   },
